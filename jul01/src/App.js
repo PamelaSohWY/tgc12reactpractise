f78d5d8ac66a72bb5c2aeacf6aae83290eeac009
PamelaSohWY <pamelasohwy@gmail.com>
2021-06-08 07:38:50 +0000

    jul01 is the tutorial with julius regarding counter

---
+++ jul01/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter.js';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+     <Counter/>
+     {/* <h1>test</h1> // to test app js is working  */}
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ jul01/src/Counter.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+export default class Counter extends React.Component{
+
+    //set state 
+    state ={
+        count: 10 
+    }
+
+    //function to add/ increase by one 
+    increment =()=>{
+       // if(this.state.count >= 15){} //state.count already display 15
+      //  {this.state.count:15} this is not necessary
+      //  else { 
+     //   this.setState(
+    //        {count: this.state.count+1}
+   //     )}
+  //  }
+   if (this.state.count<15){
+       this.setState({count: this.state.count+1})  }
+    }// end of increment
+
+
+
+    decrement =()=>{
+       
+    // evaluate first then set state 
+    if (this.state.count>0) {// compare state.count with the condition 0 
+       this.setState(
+            {count:this.state.count-1}
+        ) }//end of if 
+    }//end of decrement
+
+
+    changeColour =(c)=>{    //need to put in variable , reger to line 54, then will show c=this.state.count
+         if (c%2==0){
+            return "blue"  
+         } else {
+             return "red"
+         }
+     }
+
+     // function() no need arguement if there are global variables
+
+
+    //to display
+    render() {
+        return(
+        <React.Fragment>
+            <div style ={{  //after 2 curly brace it is in JS //first blue indicate JS //2nd curly Object (key-value pair) //1 objevct 4 keys
+                border:"2 px solid black",
+                padding:"10px",
+                width:"20px",
+                color: this.changeColour(this.state.count) //call function, put in state count as arguement
+           
+           //
+           
+           
+           }}  >            <h1>
+            {this.state.count}</h1>
+
+            <button onClick={this.increment}>+1</button>
+            <button onClick={this.decrement}>-1</button>
+
+            </div>
+        </React.Fragment>
+        )
+    } //end of render
+}// end of class counter 
+
+
+
+//Note : make sure that file name is capital 
+// if else is not understood in render, bec its html. 
+//So JS should be put into the function instead 
